39b593bf28c5e615a1c025578e6a684f3609b27a
Evgeny Larkov <elarkov@inbox.ru>
2018-07-06 20:14:19 +0700

    made the outputing data

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import 'normalize.css';
 import './scss/layout/base.scss';
 
 /*= == include handlebars template === */
-
+// import render from './templates/content.hbs';
 /*= == modules === */
 import {
   func,
 } from './js/addRow';
 
-/*= == display of table one === */
+
+/*= == display of contacts === */
+// const container = document.querySelector('.content');
+// container.innerHTML = render({ response});
+
 
 func();
--- src/js/addRow.js
+++ src/js/addRow.js
@@ -1,8 +1,34 @@
-function func(){
-  console.log('done');
+import render from '../templates/content.hbs';
+
+function func() {
+  //const button = document.querySelector('.button');
+  const result = document.querySelector('.content');
+
+  window.addEventListener('load', () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://demo.sibers.com/users');
+    xhr.send();
+    xhr.addEventListener('load', () => {
+      if (xhr.status >= 404) {
+        console.log('Что-то пошло не так');
+      } else {
+        const users = JSON.parse(xhr.responseText);
+        console.log(users);
+        // result.innerHTML = render(xhr.response);
+        for (const user of users) {
+          result.appendChild(createUsers(user));
+        }
+      }
+    });
+  });
+
+  function createUsers(user) {
+    const li = document.createElement('li');
+    li.className = 'users';
+    li.textContent = `${user.name}`;
+    return li;
+  }
 }
-
-
 
 export {
   func,
